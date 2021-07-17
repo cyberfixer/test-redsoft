85abc4b193067efb436ff17b911fad9f3ba10210
Dima Glushkov <dglushkov@mail.ru>
2021-07-17 13:26:32 +0700

    deploy.sh add

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env sh
-# abort on errors
 set -e
-# build
+
 npm run build
-# navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:k-teck/test-redsoft.git origin gh-pages
+git switch -c gh-pages
+
+git remote add origin git@github.com:k-teck/test-redsoft.git
+git push origin gh-pages
+
 cd -
